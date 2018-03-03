40d3d3c4e36ea4c5f44e0b59ae18e80d5f67e8e9
stevencharleshuang <steven.charles.huang@gmail.com>
2018-03-03 00:11:14 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+	},
+];
+
+
 var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
@@ -13,10 +36,11 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 
-console.log("User flipped " + cards[cardId]);
-
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 checkForMatch();
 };
 
