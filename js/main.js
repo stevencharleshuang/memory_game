6e618113e7d47d773f0e358afa43f520377309e1
stevench <stevencharles>
2018-03-02 19:24:24 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,19 +2,23 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1])
 	{
 	alert("You found a match!");
 	}
 	else alert("Sorry, try again.")
+	}
 }
+
+var flipCard = function(cardId) {
+cardsInPlay.push(cards[cardId]);
+
+console.log("User flipped " + cards[cardId]);
+
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
